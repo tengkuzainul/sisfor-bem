f0c91931ce1e97b5bc01f4768b40611c179b4b67
tengkuzainul <tengkuzainula04@gmail.com>
2026-06-10 18:57:32 +0700

    commant line call seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,8 @@ class DatabaseSeeder extends Seeder
             'role'  => 'administrator',
         ]);
 
-        $this->call([
-            OrganisasiSeeder::class,
-        ]);
+        // $this->call([
+        //     OrganisasiSeeder::class,
+        // ]);
     }
 }
